9fd61013f7b9a181f029696d6cde6c8f218cad64
Oliver Chalk <oliverchalk245@gmail.com>
2020-11-24 14:13:04 +1100

    Add hardware display scripts, network gui, updated use-nvcode

--- clean-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $HOME/.config/nvim/
-mkdir ../temp-backup/ || exit
-mv autoload ../temp-backup/
-mv plugged ../temp-backup/
-mv session ../temp-backup/
-rm -rf ./*
-mv ../temp-backup/autoload/* ./
-rm -rf ../temp-backup
--- use-nvcode.sh
+++ use-nvcode.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p $HOME/.config/nvim/
+mv $HOME/.config/nvim/autoload/plugged/ $HOME/.tmp.autoload.plugged.bak/
 cp -R ./nvcode/* $HOME/.config/nvim/
+mv $HOME/.tmp.autoload.plugged.bak/ $HOME/.config/nvim/autoload/plugged/
