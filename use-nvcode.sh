c5bf98279b697f38fa4e71190d65d505601e759b
Oliver Chalk <oliverchalk245@gmail.com>
2020-11-24 16:49:29 +1100

    Add .xinitrc, .editorconfig, fix use-nvcode.sh

--- use-nvcode.sh
+++ use-nvcode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p $HOME/.config/nvim/
 mv $HOME/.config/nvim/autoload/plugged/ $HOME/.tmp.autoload.plugged.bak/
+rm -rf $HOME/.config/nvim/*
 cp -R ./nvcode/* $HOME/.config/nvim/
 mv $HOME/.tmp.autoload.plugged.bak/ $HOME/.config/nvim/autoload/plugged/
