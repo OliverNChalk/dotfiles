9bf0912eb59ce1d2362011f10db486c98215d8ea
Oliver Chalk <oliverchalk245@gmail.com>
2020-11-27 01:05:46 +1100

    Add startup to fish config, update hardware xrdb settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,21 @@ alias ls="ls --color=auto"
 set --export EDITOR nvim
 set --export TERMINAL alacritty
 
+# Functions
 function nvm
     bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 
 function vimdiff
     nvim -d $argv
+end
+
+# Run on startup
+if status is-login
+    nvm use default
+
+    # If $DISLAY == 0 && $XDG_VTNR == 1
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
 end
